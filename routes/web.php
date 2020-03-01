d4055716fd6b52367f4da5481b851219874b5cbf
fuzzy-yusuke <youth_k0228@outlook.com>
2020-03-02 00:16:24 +0900

    #5

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ Route::get('/myspots','PlaceController@index')->name('place.list');
 //一覧ページのルーティングを定義
 //ドメインの最後が「myspots」の時、PlaceControllerを呼び出す
 
+Route::get('/myspot/{id}','PlaceController@show')->name('place.detail');
+//詳細ページへのルーティング
+//idを引数にし、各スポットの詳細ページを呼び出す
+
 Route::get('/', function () {
     return redirect('/myspots');
 });
