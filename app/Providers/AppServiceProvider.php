03527f6efc5735c45db23fc046e08a7091453c61
fuzzy-yusuke <youth_k0228@outlook.com>
2020-04-06 23:19:21 +0900

    function boot comentout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //URLが「https」から始まるようにする
-      //  \URL::forceScheme('https');
+        //
     }
 }
