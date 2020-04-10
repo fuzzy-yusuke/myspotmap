c29a06d31a089d5c9022d4340052a3b265ba197d
fuzzy-yusuke <youth_k0228@outlook.com>
2020-04-11 00:12:35 +0900

    データ型の変更

--- database/migrations/2020_04_08_140246_add_user_id_to_places_table.php
+++ database/migrations/2020_04_08_140246_add_user_id_to_places_table.php
@@ -14,7 +14,7 @@ class AddUserIdToPlacesTable extends Migration
     public function up()
     {
         Schema::table('places', function (Blueprint $table) {
-            $table->string('user_id');
+            $table->integer('user_id');
         });
     }
 
